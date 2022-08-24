0f2aa6a2382432cb1c0f7bce67b0af11fa9c790f
Alexandre Ferrarini <alexandreferrarini@gmail.com>
2022-08-24 20:05:14 -0300

    pratica-1.js versão 4

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,7 @@ let idade = 40
 let lugarConhecer = "Japão"
 
 console.log(`${nome}\n${email}\n${profissao}\n${idade}\n${lugarConhecer}`)
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarConhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, 
+você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
