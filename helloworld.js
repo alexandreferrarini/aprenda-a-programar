9ffd214a889885156cea2bf6a3d3816cac1c35f7
Alexandre Ferrarini <alexandreferrarini@gmail.com>
2022-08-24 17:16:34 -0300

    commit tarefa 2 etapa 1

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,10 @@
 console.log("hello world");
-console.log("Olá mundo");
-console.log("halo dunia");
+//console.log("Olá mundo");
+//console.log("halo dunia");
+
+let minhavariavel
+let outravariavel = 1
+minhavariavel = "ITuring"
+
+console.log(minhavariavel)
+console.log(outravariavel)
