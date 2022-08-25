41fbe2cefa62b7a26f122e2db74f2461b8e22111
Alexandre Ferrarini <alexandreferrarini@gmail.com>
2022-08-25 17:37:52 -0300

    tarefa 2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let numero = 9;
+let i = 1;
+let numeroLimite = 10;
+
+console.log(`Tabuada de ${numero}`);
+
+while (i <= numeroLimite) {
+    console.log(`${numero} X ${i} = ${numero*i}`);
+    i++;
+}
